bc0c23263718e299e60c1f11da58d9d12f91b89d
Shun Usami <usatie@gmail.com>
2022-03-26 10:21:14 +0900

    :sparkles: Add sym link script

---
+++ projects/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+ln -s ~/Desktop/nand2tetris/tools/JackCompiler.sh /usr/local/bin/JackCompiler
+ln -s ~/Desktop/nand2tetris/tools/Assembler.sh /usr/local/bin/Assembler
+ln -s ~/Desktop/nand2tetris/tools/HardwareSimulator.sh /usr/local/bin/HardwareSimulator
+ln -s ~/Desktop/nand2tetris/tools/VMEmulator.sh /usr/local/bin/VMEmulator
+ln -s ~/Desktop/nand2tetris/tools/CPUEmulator.sh /usr/local/bin/CPUEmulator
